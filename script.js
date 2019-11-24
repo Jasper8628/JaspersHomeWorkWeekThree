baebd42da57d8323b0c819e8de657151c3fb95d8
jasper <msavantfou@gmail.com>
2019-11-24 20:20:19 +1100

    addEventListener now functional

--- script.js
+++ script.js
@@ -1,83 +1,89 @@
-var input=prompt("How many characters would you likey your password to contain?");
-var userInput=parseInt(input);
-console.log( input);
-console.log(userInput);
-while(input==null|| isNaN(input)|| userInput<8||userInput>128){
-  alert("Please provide a number between 8 and 128");
-  input=prompt("How many characters would you likey your password to contain?");
-  userInput=parseInt(input);
-  
-}
-var includeCapitalLetters=confirm("Would your like your password to contain capital letters?");
-var includeNumbers=confirm("Would you like your password to contain numbers?");
-var includeSymbols=confirm("Would you like your password to contain special symbols?");
+var generatePassword = document.querySelector("#generate-password");
+var copyToClipboard = document.querySelector("#copy-click-board");
+var displayPassword = document.querySelector("#password");
+generatePassword.addEventListener("click", function (event) {
+  event.preventDefault();
 
+  var input = prompt("How many characters would you likey your password to contain?");
+  var userInput = parseInt(input);
 
+  while (input == null || isNaN(input) || userInput < 8 || userInput > 128) {
+    alert("Please provide a number between 8 and 128");
+    input = prompt("How many characters would you likey your password to contain?");
+    userInput = parseInt(input);
+  }
+  var includeCapitalLetters = confirm("Would your like your password to contain capital letters?");
+  var includeNumbers = confirm("Would you like your password to contain numbers?");
+  var includeSymbols = confirm("Would you like your password to contain special symbols?");
+  var defaultCharacterPool = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var capitalLetterPool = [];
+  var numbersPool = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  var symbolPool = [",", ".", "/", "<", ">", "?", ";", "'", ":", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "="];
+  var newPassword = [];
+  var charPool = [];
 
-var defaultCharacterPool=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var capitalLetterPool=[];
-var numbersPool=["0","1","2","3","4","5","6","7","8","9"]
-var symbolPool=[",",".","/","<",">","?",";","'",":","~","!","@","#","$","%","^","&","*","(",")","_","+","-","="];
-var newPassword=[];
-var charPool=[];
-var generate=document.getElementById("generate");
-var copy=document.querySelector("#copy");
-var displayPassword=document.querySelector("#password");
-for(i=0;i<26;i++){
-  var getLetter=defaultCharacterPool[i].toUpperCase();
-  capitalLetterPool.push(getLetter);
-}
-//console.log(capitalLetterPool);
+  for (i = 0; i < 26; i++) {
+    var getLetter = defaultCharacterPool[i].toUpperCase();
+    capitalLetterPool.push(getLetter);
+  }
 
-
-
-for(i=0;i<userInput;i++){
-  if(includeCapitalLetters){
-    if(includeNumbers){
-      if(includeSymbols){
-        charPool=defaultCharacterPool.concat(capitalLetterPool);
-        charPool=charPool.concat(numbersPool);
-        charPool=charPool.concat(symbolPool);
-        //console.log(charPool);
+  for (i = 0; i < userInput; i++) {
+    if (includeCapitalLetters) {
+      if (includeNumbers) {
+        if (includeSymbols) {
+          charPool = defaultCharacterPool.concat(capitalLetterPool);
+          charPool = charPool.concat(numbersPool);
+          charPool = charPool.concat(symbolPool);
+          //console.log(charPool);
+        }
+        else {
+          charPool = defaultCharacterPool.concat(capitalLetterPool);
+          charPool = charPool.concat(numbersPool);
+        }
       }
-      else{
-        charPool=defaultCharacterPool.concat(capitalLetterPool);
-        charPool=charPool.concat(numbersPool);
+      else if (includeSymbols) {
+        charPool = defaultCharacterPool.concat(capitalLetterPool);
+        charPool = charPool.concat(symbolPool);
+      }
+      else {
+        charPool = defaultCharacterPool.concat(capitalLetterPool);
       }
     }
-    else if(includeSymbols){
-      charPool=defaultCharacterPool.concat(capitalLetterPool);
-      charPool=charPool.concat(symbolPool);
+    else if (includeNumbers) {
+      if (includeSymbols) {
+        charPool = defaultCharacterPool.concat(numbersPool);
+        charPool = charPool.concat(symbolPool);
+      }
+      else {
+        charPool = defaultCharacterPool.concat(numbersPool);
+      }
     }
-    else{
-      charPool=defaultCharacterPool.concat(capitalLetterPool);
+    else if (includeSymbols) {
+      charPool = defaultCharacterPool.concat(symbolPool);
     }
-  }
-  else if(includeNumbers){
-    if(includeSymbols){
-      charPool=defaultCharacterPool.concat(numbersPool);
-      charPool=charPool.concat(symbolPool);
+    else {
+      charPool = defaultCharacterPool;
     }
-    else{
-      charPool=defaultCharacterPool.concat(numbersPool);
-    }
-  }
-  else if(includeSymbols){
-    charPool=defaultCharacterPool.concat(symbolPool);
-  }
-  else{
-    charPool=defaultCharacterPool;
-  }
 
-  var charIndex=Math.floor((Math.random()*charPool.length));
-  var newChar=charPool[charIndex];
-  newPassword.push(newChar);
-  console.log(newChar);
-  
-}
-var finalPassword= newPassword.join("");
-console.log(finalPassword);
-//alert("Here's your new password: "+finalPassword);
-displayPassword.value=finalPassword;
+    var charIndex = Math.floor((Math.random() * charPool.length));
+    var newChar = charPool[charIndex];
+    newPassword.push(newChar);
+    //console.log(newChar);
+
+  }
+  var finalPassword = newPassword.join("");
+  //console.log(finalPassword);
+  //alert("Here's your new password: "+finalPassword);
+  displayPassword.value = finalPassword;
+
+
+});
+
+copyToClipboard.addEventListener("click", function (event) {
+  event.preventDefault();
+  displayPassword.select();
+  document.execCommand("copy");
+  alert("Password copied to clipboard as: " + displayPassword.value);
+});
 
 
